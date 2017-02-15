0a9f0af2fdef60e208ae2696645b10641d6eb3a5
AlexYaruki <alexyaruki@gmail.com>
2017-02-15 15:49:32 +0100

    Fix to "go" bindings build errors

--- bindings/go/src/gotoc/avg.go
+++ bindings/go/src/gotoc/avg.go
@@ -1,13 +1,13 @@
 package gotoc
 
 import (
-	
-	
-	
+
+
+
 )
 /*
 
-#include "../../../../../Include/sigar.h"
+#include "../../../../include/sigar.h"
 
 
 
@@ -17,13 +17,13 @@ import "C"
 func Avg()C.sigar_loadavg_t{
 
 	var sigar *C.sigar_t=GetSigarHandle()
-	
+
 	var avg C.sigar_loadavg_t
-	
-	
+
+
 	C.sigar_loadavg_get(sigar, &avg)
-	
-	
+
+
 	return avg
 
 }
--- bindings/go/src/gotoc/resource.go
+++ bindings/go/src/gotoc/resource.go
@@ -1,25 +1,19 @@
 package gotoc
 
-import (
-	
-	
-	
-)
 /*
-#include "../../../../../Include/sigar.h"
+#include "../../../../include/sigar.h"
 
 
 
 */
 import "C"
 
-func Resources()C.sigar_resource_limit_t{
-	
+func Resources() C.sigar_resource_limit_t {
+
 	var resources C.sigar_resource_limit_t
-	
+
 	C.sigar_resource_limit_get(sigar, &resources)
-	
-	
+
 	return resources
 
 }
